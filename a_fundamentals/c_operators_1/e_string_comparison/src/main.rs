9ce2dda32c36b8ce90bee541a7180a264016a0b8
Ventures <admin@geminiventures.io>
2026-03-12 10:22:40 -0400

    str and String comparison

--- a_fundamentals/c_operators_1/e_string_comparison/src/main.rs
+++ a_fundamentals/c_operators_1/e_string_comparison/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    // all 3 are valid ways to create a String
+    let string1: String = "hello".to_string();
+    let string2: String = String::from("hello");
+    let string3 = "hello".to_owned();
+    println!("{} {} {}", string1, string2, string3);
 }
