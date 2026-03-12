4c6f53b767e33ffa8352a272a606eda51057e36c
Ventures <admin@geminiventures.io>
2026-03-12 10:20:47 -0400

    Arethmetic operators

--- a_fundamentals/c_operators_1/a_arithmetic_operators/src/main.rs
+++ a_fundamentals/c_operators_1/a_arithmetic_operators/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // Type your code below
+    let a = 5.2;
+    let b = 2.6;
+    let c:f64 = a / b;
+
+    // Don't change the line below
+    println!("a = {}, b = {}, c = {}", a, b, c);
 }
