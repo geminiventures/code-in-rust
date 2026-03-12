435d4ae14a8525e4fc9f6219a07d46b13e007819
Ventures <admin@geminiventures.io>
2026-03-12 12:49:05 -0400

    Logical Operators

--- a_fundamentals/d_operators_2/a_logical_operators_1/src/main.rs
+++ a_fundamentals/d_operators_2/a_logical_operators_1/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // Type your code below
+    let b1: bool = true;
+    let b2: bool = false;
+    let b3: bool = b1 || b2;
+
+    // Don't change the line below
+    println!("b3 = {}", b3);
 }
