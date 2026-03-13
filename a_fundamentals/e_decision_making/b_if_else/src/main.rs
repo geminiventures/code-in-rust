045599b4eaf8b249a75fd4910b5e130eb4312791
Ventures <admin@geminiventures.io>
2026-03-12 23:57:01 -0400

    If Else

--- a_fundamentals/e_decision_making/b_if_else/src/main.rs
+++ a_fundamentals/e_decision_making/b_if_else/src/main.rs
@@ -1,3 +1,16 @@
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let wind: i32 = input.trim().parse().unwrap();
+    // Replace the line below with an if-else expression that assigns status based on wind
+    let status = if wind < 8 { "Calm" }
+    else if wind <= 31 { "Breeze" }
+    else if wind <= 63 { "Gale" }
+    else {  "Storm" };
+
+    // Don't change the line below
+    println!("status = {}", status);
 }
