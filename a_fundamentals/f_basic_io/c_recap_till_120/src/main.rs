61b73ce6394202fe9cbc9f61548afdf1461e3523
Ventures <admin@geminiventures.io>
2026-03-13 00:57:16 -0400

    Recap: convert user input to u32

--- a_fundamentals/f_basic_io/c_recap_till_120/src/main.rs
+++ a_fundamentals/f_basic_io/c_recap_till_120/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let age: u32 = input.trim().parse().unwrap();
+    println!("{} years till 120", 120 - age);
 }
