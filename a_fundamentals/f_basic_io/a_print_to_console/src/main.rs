28645686be207c8316936f7560e437bc4589886f
Ventures <admin@geminiventures.io>
2026-03-13 00:55:40 -0400

    println! and print!

--- a_fundamentals/f_basic_io/a_print_to_console/src/main.rs
+++ a_fundamentals/f_basic_io/a_print_to_console/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let hello = "Hello, ";
+    let coddy = "Coddy!";
+    let rust_is_fun = "Rust is fun!";
+    // Write your code below
+
+    println!("{}{}", hello, coddy);
+    println!("{}", rust_is_fun);
 }
